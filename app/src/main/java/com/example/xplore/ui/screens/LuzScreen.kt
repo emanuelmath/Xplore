bb7a5a5852b81beb5581e396f01bbd27be2e0855
LeslieEspinoza <em234100156@uma.edu.sv>
2025-10-20 01:04:11 -0600

    agregue contenido en la screen de luz

--- app/src/main/java/com/example/xplore/ui/screens/LuzScreen.kt
+++ app/src/main/java/com/example/xplore/ui/screens/LuzScreen.kt
@@ -1,2 +1,99 @@
 package com.example.xplore.ui.screens
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.xplore.R
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LuzScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 40.dp)
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.luz),
+                    contentDescription = "Icono luz",
+                    modifier = Modifier.size(48.dp)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "PANTALLA ADAPTIVA",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+            Box(
+                modifier = Modifier
+                    .background(Color(0xFF4B5D7C), shape = RoundedCornerShape(10.dp))
+                    .padding(24.dp)
+                    .width(260.dp)
+                    .height(400.dp),
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.sol),
+                        contentDescription = "Clima nublado",
+                        modifier = Modifier.size(100.dp)
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Text(
+                        text = "ACTUALMENTE ESTAS EN MODO CLARO",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewLuzScreen() {
+    LuzScreen()
+}
